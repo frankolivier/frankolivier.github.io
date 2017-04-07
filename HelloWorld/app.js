d66e2c87349177bc7bd72288fdec75b16185a626
frankolivier <franko@ntdev.microsoft.com>
2017-04-06 17:25:56 -0700

    adding three.js

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -28,6 +28,47 @@ var needToRender = true;
 
 var cachedTiles = [];
 
+
+/// three js
+var scene, camera, renderer;
+var geometry, material, mesh;
+
+
+
+function initThree() {
+
+	scene = new THREE.Scene();
+
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 1000;
+
+	geometry = new THREE.BoxGeometry( 200, 200, 200 );
+	material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animateThree() {
+
+	requestAnimationFrame( animateThree );
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+
+}
+/// three js
+
+
+
 function getTileId(x, y) {
 	return x + y * tileDimension;
 }
@@ -191,6 +232,11 @@ function onWindowResize()
 }
 
 function init() {
+
+	initThree();
+	animateThree();
+	return;
+	
 	canvas = document.getElementById('outputCanvas');
 	ctx = canvas.getContext('2d');
 
